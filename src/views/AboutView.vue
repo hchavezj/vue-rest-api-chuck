a9f0bb5b5e90e0b2d918290bf92878bad1fefbd7
hchavezj <hchavezj@gmail.com>
2022-10-08 01:50:53 -0500

    Chuck Norris

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Just get some facts about Chuck!</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>👊 Chuck Norris Fact</h1>
+    <div v-if="chuckFact" class="fact">
+      <i>"{{ chuckFact }}"</i>
+    </div>
+    <button @click="showFact()">Get Fact</button>
   </div>
 </template>
 
-<script>
+<script scope>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import axios from "axios";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      chuckFact: "",
+    };
+  },
+  methods: {
+    async showFact() {
+      let config = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+      const chuckFact = await axios.get(
+        "https://api.chucknorris.io/jokes/random",
+        config
+      );
+      this.chuckFact = chuckFact.data.value;
+    },
   },
 };
 </script>
+
+<style scope>
+.fact {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  font-size: 1.5rem;
+}
+.home button {
+  font-size: 1.2rem;
+  background-color: whitesmoke;
+  border: 1px, solid, black;
+  color: black;
+  padding: 5px 15px;
+  text-align: center;
+  /*text-decoration: none;*/
+  display: inline-block;
+}
+</style>
